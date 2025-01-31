80855578c9900d21d6c58696a833a6abe686aeb5
brunomachado37 <brunomachadocarneiro37@gmail.com>
2025-01-31 10:42:25 +0100

    Adapt code to work with 2 cameras instead of 3. Additional cameras can be added using the info.py file.

--- droid/user_interface/data_collector.py
+++ droid/user_interface/data_collector.py
@@ -82,8 +82,6 @@ class DataCollecter:
             save_filepath = None
             recording_folderpath = None
         else:
-            if len(self.full_cam_ids) != 6:
-                raise ValueError("WARNING: User is trying to collect data without all three cameras running!")
             save_filepath = os.path.join(self.failure_logdir, info["time"], "trajectory.h5")
             recording_folderpath = os.path.join(self.failure_logdir, info["time"], "recordings")
             if not os.path.isdir(recording_folderpath):
