4ccd1a1066f9eb622d587ea359299688dd6c466d
brunomachado37 <brunomachadocarneiro37@gmail.com>
2025-01-31 10:34:23 +0100

    Set parameters

--- droid/misc/parameters.py
+++ droid/misc/parameters.py
@@ -2,23 +2,23 @@ import os
 from cv2 import aruco
 
 # Robot Params #
-nuc_ip = ""
-robot_ip = ""
-laptop_ip = ""
-sudo_password = ""
-robot_type = ""  # 'panda' or 'fr3'
-robot_serial_number = ""
+nuc_ip = None
+robot_ip = "192.168.10.100"
+laptop_ip = "192.168.10.4"
+sudo_password = "panda"
+robot_type = "panda"  # 'panda' or 'fr3'
+gripper_type = "franka" # 'franka' or 'robotiq'
+robot_serial_number = "295341-1325717"
 
 # Camera ID's #
-hand_camera_id = ""
-varied_camera_1_id = ""
-varied_camera_2_id = ""
+hand_camera_id = "15483906"
+varied_camera_1_id = "21555878"
 
 # Charuco Board Params #
 CHARUCOBOARD_ROWCOUNT = 9
 CHARUCOBOARD_COLCOUNT = 14
-CHARUCOBOARD_CHECKER_SIZE = 0.020
-CHARUCOBOARD_MARKER_SIZE = 0.016
+CHARUCOBOARD_CHECKER_SIZE = 0.015
+CHARUCOBOARD_MARKER_SIZE = 0.011
 ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_100)
 
 # Ubuntu Pro Token (RT PATCH) #
